a4d274766f35e428ce30cf4fa68d6c57682b7ce1
Juan Jose Araujo Bedon <juan.araujo.bedon@udla.edu.ec>
2023-11-22 08:03:04 -0500

    Agregar archivos de proyecto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiApp_JuanJoseAraujo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
